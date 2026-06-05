7d22125c74a7494691668b27ecc909f8f176c394
JulianChavezDev <julicgavez1@gmail.com>
2026-06-05 11:46:04 +0200

    adding img

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,28 @@
+const Hero = () => {
+    return (
+        <section id ="hero" className="noisy">
+            <h1 className="title"> MOJITO </h1>
+
+            <img
+                src="public/images/hero-left-leaf.png"
+                alt="left-leaf"
+                className="left-leaf"
+            />
+
+            <img
+                src="public/images/hero-right-leaf.png"
+                alt="right-leaf"
+                className="right-leaf"
+            />
+            <div className={} name="body">
+                <div className="content">
+                    <div className="space-y-5 hidden md:block">
+                        <p>Cool. Crisp. Classic</p>
+                        <p className="subtitle"></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Hero;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,48 @@
+import {navLinks} from "../../constants/index.js";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+
+const Navbar = () => {
+    useGSAP(() => {
+
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top',
+                scrub: true,
+            }
+        });
+
+
+        navTween.fromTo('nav',
+            {backgroundColor: 'transparent'}, {
+                backgroundColor: '#00000005',
+                backgroundFilter: 'blur(10px)',
+                duration: 1,
+                ease: 'power1.inOut'
+            });
+    })
+
+
+    return (
+        <nav>
+        <div>
+            <a href="#home" className="flex-items-center gap-2">
+                <img src='public/images/logo.png' alt='logo'/>
+                <p>velvet Pour</p>
+            </a>
+            <ul>
+                {navLinks.map(link => (
+                    <li key={link.id}>
+                        <a href={`#${link.id}`}> {link.title}</a>
+
+                    </li>
+                ))}
+            </ul>
+        </div>
+        </nav>
+    );
+};
+
+export default Navbar;
